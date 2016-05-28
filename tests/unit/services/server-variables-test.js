4821b4d645a80dfa3f02010a9c8c8f7501af8f82
Ben Limmer <ben@ibotta.com>
2016-05-28 12:48:04 -0600

    Support parsing JSON from server tags. (#16)
    Resolves #15.

--- tests/unit/services/server-variables-test.js
+++ tests/unit/services/server-variables-test.js
@@ -11,43 +11,50 @@ import ENV from 'dummy/config/environment';
 
 moduleFor('service:server-variables', {
   // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+  beforeEach() {
+    setPrefix(ENV.serverVariables.tagPrefix);
+  }
 });
 
 test('it returns the tag content when found', function(assert) {
-  setPrefix(ENV.serverVariables.tagPrefix);
   addHeadTag('service-test-location', 'Denver');
 
-  var service = this.subject();
+  const service = this.subject();
   assert.equal(service.get('serviceTestLocation'), 'Denver');
 
   cleanupHeadTag('service-test-location');
 });
 
 test('it returns undefined when the content is not found', function(assert) {
-  setPrefix(ENV.serverVariables.tagPrefix);
-
-  var service = this.subject();
+  const service = this.subject();
   assert.equal(service.get('nonExistant'), undefined);
 });
 
 test('it returns undefined when content is not set', function(assert) {
-  setPrefix(ENV.serverVariables.tagPrefix);
   addHeadTag('service-test-foo', '');
 
-  var service = this.subject();
+  const service = this.subject();
   assert.equal(service.get('serviceTestFoo'), undefined);
 
   cleanupHeadTag('service-test-foo');
 });
 
 test('it accepts dasherized or camelcased', function(assert) {
-  setPrefix(ENV.serverVariables.tagPrefix);
   addHeadTag('service-test-foo', 'something');
 
-  var service = this.subject();
+  const service = this.subject();
   assert.equal(service.get('serviceTestFoo'), 'something');
   assert.equal(service.get('service-test-foo'), 'something');
 
   cleanupHeadTag('service-test-foo');
+});
+
+test('it parses JSON tags', function(assert) {
+  addHeadTag('service-test-json', JSON.stringify({ foo: 'foo', bar: 'bar' }));
+
+  const service = this.subject();
+  const res = service.get('service-test-json');
+  assert.deepEqual(res, { foo: 'foo', bar: 'bar' });
+  assert.equal(typeof res, 'object');
+  assert.notEqual(typeof res, 'string');
 });
